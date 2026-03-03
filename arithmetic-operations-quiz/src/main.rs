78c95dd2eaaf6fab60be6ee5e09b59d9d8f0e26e
k07g <korenaga.makoto@ea-sys.jp>
2026-03-03 21:25:50 +0900

    標準入力を追加

--- arithmetic-operations-quiz/src/main.rs
+++ arithmetic-operations-quiz/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("1 + 1 = ??");
+    println!("??の値を入力して下さい:");
+    let mut ans_input = String::new();
+    std::io::stdin().read_line(&mut ans_input).unwrap();
+    dbg!(ans_input);
 }
